1e6fbff7a194a269453f8f28ee71cda36e09b644
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2022-11-06 17:04:55 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos</h1>
+        <Testimonio 
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='Emma'
+          descFoto='Foto de Emma'
+          cargo='Ingeniero de Software'
+          empresa='Spotify'
+          testimonio='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni laborum aut eligendi, quam tempore qui natus, nam sunt quae earum consequuntur sint? Eos labore iusto at atque, non hic exercitationem!' />
+        <Testimonio
+          nombre='Shawn Pepito'
+          pais='Singapur'
+          imagen='Juan'
+          descFoto="Foto de Juan"
+          cargo='Ingeniero de Software'
+          empresa='Amazon'
+          testimonio='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni laborum aut eligendi, quam tempore qui natus, nam sunt quae earum consequuntur sint? Eos labore iusto at atque, non hic exercitationem!' />
+        <Testimonio
+          nombre='Sara Chima'
+          pais='Nigeria'
+          imagen='Sara'
+          descFoto='Foto de Sara'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni laborum aut eligendi, quam tempore qui natus, nam sunt quae earum consequuntur sint? Eos labore iusto at atque, non hic exercitationem!' />
+      </div>
+      
     </div>
   );
 }
